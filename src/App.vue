2f2c82af47350c5f85743297424a8c1ccd92b4aa
aymanElkhouly <elkhouly.ayman@outlook.com>
2021-07-10 01:48:36 +0200

    Init project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <section class="layout container">
+    <header class="mt-5 mb-2">
+      <h1 class="mb-3">Booking System</h1>
+      <div class="divider"></div>
+    </header>
+    <router-view/>
+  </section>
+</template>
+
+<style lang="scss"></style>
---
+++ src/store/index.js
@@ -0,0 +1,13 @@
+import { createStore } from 'vuex'
+import bookingStore from '@/modules/booking/store'
+export default createStore({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    bookingStore
+  }
+})

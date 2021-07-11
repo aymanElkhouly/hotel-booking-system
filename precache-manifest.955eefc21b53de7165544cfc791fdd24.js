986943142617d3e177501b8a99adbc464fc2c987
aymanElkhouly <elkhouly.ayman@outlook.com>
2021-07-11 19:57:46 +0200

    deploy

---
+++ precache-manifest.955eefc21b53de7165544cfc791fdd24.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e784b6d743818a8fa2e8",
+    "url": "/hotel-booking-system/css/app.6ecb6fba.css"
+  },
+  {
+    "revision": "01330192c7a0c4fe5dd5",
+    "url": "/hotel-booking-system/css/chunk-vendors.9d8ae10a.css"
+  },
+  {
+    "revision": "7fb23f3b56a834d9dcf615b5e7c78da8",
+    "url": "/hotel-booking-system/fonts/bootstrap-icons.7fb23f3b.woff"
+  },
+  {
+    "revision": "e07b538aa51b6fa77f32828af21cb591",
+    "url": "/hotel-booking-system/fonts/bootstrap-icons.e07b538a.woff2"
+  },
+  {
+    "revision": "d1eea9678738cb93d5601f1c25d9c86d",
+    "url": "/hotel-booking-system/index.html"
+  },
+  {
+    "revision": "e784b6d743818a8fa2e8",
+    "url": "/hotel-booking-system/js/app.e42bdc11.js"
+  },
+  {
+    "revision": "83b3831e239c9e7496c4",
+    "url": "/hotel-booking-system/js/booking.02b1b217.js"
+  },
+  {
+    "revision": "01330192c7a0c4fe5dd5",
+    "url": "/hotel-booking-system/js/chunk-vendors.f355312a.js"
+  },
+  {
+    "revision": "7223cb5512a393e159cf41eac62b0271",
+    "url": "/hotel-booking-system/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hotel-booking-system/robots.txt"
+  }
+]);
